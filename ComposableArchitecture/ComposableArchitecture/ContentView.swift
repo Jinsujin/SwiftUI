fe6e9fa2b02ba23ae903ffd36f3ac3be885b6761
Jinsujin <sylpid003@gmail.com>
2023-04-01 17:01:56 +0900

    feat: request API  (#9)

--- ComposableArchitecture/ComposableArchitecture/ContentView.swift
+++ ComposableArchitecture/ComposableArchitecture/ContentView.swift
@@ -15,6 +15,17 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("State management")
+            .toolbar {
+                ToolbarItem {
+                    Button {
+                        nthPrime(1_000_000) { p in // p == Int?
+                            print(p)
+                        }
+                    } label: {
+                        Text("Prime Request")
+                    }
+                }
+            }
         }
     }
 }
@@ -25,15 +36,22 @@ private func ordinal(_ n: Int) -> String {
     return formatter.string(for: n) ?? ""
 }
 
-
 class AppState: ObservableObject {
     @Published var count = 0
     @Published var favoritePrimes: [Int] = []
 }
 
+struct PrimeAlert: Identifiable {
+    let prime: Int
+    
+    var id: Int { self.prime }
+}
+
 struct CounterView: View {
     @ObservedObject var state: AppState
     @State var isPrimeModalShown: Bool = false
+    @State var alertNthPrime: PrimeAlert?
+    @State var isNthPrimeButtonDisabled = false
     
     var body: some View {
         
@@ -55,25 +73,44 @@ struct CounterView: View {
             Button(action: { self.isPrimeModalShown = true }) {
                 Text("is this prime?")
             }
-            Button(action: {}) {
+            Button {
+                nthPrimeButtonAction()
+            } label: {
                 Text("What is the \(ordinal(self.state.count)) prime?")
             }
+            .disabled(self.isNthPrimeButtonDisabled)
         }
         .font(.title)
         .navigationTitle("Counter Demo")
         .sheet(isPresented: self.$isPrimeModalShown) {
             IsPrimeModalView(state: self.state)
         }
+        // alertNthPrime 에 값이 있을때, alert 을 띄운다
+        .alert(item: self.$alertNthPrime) { alert in
+            Alert(
+                title: Text("The \(ordinal(self.state.count)) prime is \(alert.prime)"),
+                dismissButton: .default(Text("Ok"))
+            )
+        }
+    }
+    
+    func nthPrimeButtonAction() {
+        self.isNthPrimeButtonDisabled = true
+        nthPrime(self.state.count) { prime in
+            print("response::", prime)
+            self.alertNthPrime = prime.map(PrimeAlert.init(prime:))
+            self.isNthPrimeButtonDisabled = false
+        }
     }
 }
 
 private func isPrime (_ p: Int) -> Bool {
-  if p <= 1 { return false }
-  if p <= 3 { return true }
-  for i in 2...Int(sqrtf(Float(p))) {
-    if p % i == 0 { return false }
-  }
-  return true
+    if p <= 1 { return false }
+    if p <= 3 { return true }
+    for i in 2...Int(sqrtf(Float(p))) {
+        if p % i == 0 { return false }
+    }
+    return true
 }
 
 struct IsPrimeModalView: View {
@@ -86,14 +123,14 @@ struct IsPrimeModalView: View {
                 if state.favoritePrimes.contains(self.state.count) {
                     Button {
                         state.favoritePrimes
-                        .removeAll(where: { $0 == self.state.count })
+                            .removeAll(where: { $0 == self.state.count })
                     } label: {
                         Text("Remove favorite primes")
                     }
                 } else {
                     Button {
                         state.favoritePrimes
-                        .append(self.state.count)
+                            .append(self.state.count)
                     } label: {
                         Text("Save favorite primes")
                     }
@@ -104,6 +141,68 @@ struct IsPrimeModalView: View {
         }
     }
 }
+
+
+struct WolframAlphaResult: Decodable {
+    let queryresult: QueryResult
+    
+    struct QueryResult: Decodable {
+        let pods: [Pod]
+        
+        struct Pod: Decodable {
+            let primary: Bool?
+            let subpods: [SubPod]
+            
+            struct SubPod: Decodable {
+                let plaintext: String
+            }
+        }
+    }
+}
+
+private let wolframAlphaApiKey = "??"
+
+func wolframAlpha(query: String, callback: @escaping (WolframAlphaResult?) -> Void) -> Void {
+    var components = URLComponents(string: "https://api.wolframalpha.com/v2/query")!
+    components.queryItems = [
+        URLQueryItem(name: "input", value: query),
+        URLQueryItem(name: "format", value: "plaintext"),
+        URLQueryItem(name: "output", value: "JSON"),
+        URLQueryItem(name: "appid", value: wolframAlphaApiKey),
+    ]
+    
+    URLSession.shared.dataTask(with: components.url(relativeTo: nil)!) { data, response, error in
+        callback(
+            data
+                .flatMap { try? JSONDecoder().decode(WolframAlphaResult.self, from: $0) }
+        )
+    }
+    .resume()
+}
+
+// n: 쿼리에 보낼 숫자값 , callback: API 호출후에 가져올 결과값
+func nthPrime(_ n: Int, callback: @escaping (Int?) -> Void) -> Void {
+    wolframAlpha(query: "prime \(n)") { result in
+        callback(
+            result
+                .flatMap {
+                    $0.queryresult // $0 == WolframAlphaResult
+                        .pods
+                        .first(where: { $0.primary == .some(true) })?
+                        .subpods
+                        .first?
+                        .plaintext
+                }
+                .flatMap(Int.init)
+        )
+    }
+}
+
+// Optional(15485863)
+//nthPrime(1_000_000) { p in // p == Int?
+//  print(p)
+//}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
