f5d412465180f197d5c775bc440677ba49b09301
Jinsujin <sylpid003@gmail.com>
2023-04-01 16:03:45 +0900

    SwiftUIState: ObservableObject 를 사용해 global property 관리  (#9)

--- ComposableArchitecture/ComposableArchitecture/ContentView.swift
+++ ComposableArchitecture/ComposableArchitecture/ContentView.swift
@@ -1,26 +1,70 @@
-//
-//  ContentView.swift
-//  ComposableArchitecture
-//
-//  Created by Sujin Jin on 2023/04/01.
-//
-
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    @ObservedObject var state: AppState
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                NavigationLink(destination: CounterView(state: state)) {
+                    Text("Counter Demo")
+                }
+                NavigationLink(destination: EmptyView()) {
+                    Text("Favorite Primes")
+                }
+            }
+            .navigationTitle("State management")
         }
-        .padding()
+    }
+}
+
+private func ordinal(_ n: Int) -> String {
+  let formatter = NumberFormatter()
+  formatter.numberStyle = .ordinal
+  return formatter.string(for: n) ?? ""
+}
+
+
+class AppState: ObservableObject {
+    @Published var count = 0
+}
+
+struct CounterView: View {
+    @ObservedObject var state: AppState
+    
+     var body: some View {
+         
+        VStack {
+            HStack {
+                Button {
+                    self.state.count -= 1
+                } label: {
+                    Text("-")
+                }
+                Text("\(self.state.count)")
+                Button {
+                    self.state.count += 1
+                } label: {
+                    Text("+")
+                }
+            }
+            
+            Button(action: {}) {
+                Text("is this prime?")
+            }
+            Button(action: {}) {
+                Text("What is the \(ordinal(self.state.count)) prime?")
+            }
+            
+        }
+        .font(.title)
+        .navigationTitle("Counter Demo")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(state: AppState())
     }
 }
