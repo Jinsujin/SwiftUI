62b4938e77cf093f97a5f23fc77f56492bf889be
Jinsujin <sylpid003@gmail.com>
2023-04-01 14:19:28 +0900

    init: create project (#9)

---
+++ ComposableArchitecture/ComposableArchitecture/ComposableArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableArchitectureApp.swift
+//  ComposableArchitecture
+//
+//  Created by Sujin Jin on 2023/04/01.
+//
+
+import SwiftUI
+
+@main
+struct ComposableArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
