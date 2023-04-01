8ffedab985bbb84a8bd8be5418bb13ab54537aba
Jinsujin <sylpid003@gmail.com>
2023-04-01 16:20:01 +0900

    feat: save/remove prime  (#9)

--- ComposableArchitecture/ComposableArchitecture/ContentView.swift
+++ ComposableArchitecture/ComposableArchitecture/ContentView.swift
@@ -20,21 +20,23 @@ struct ContentView: View {
 }
 
 private func ordinal(_ n: Int) -> String {
-  let formatter = NumberFormatter()
-  formatter.numberStyle = .ordinal
-  return formatter.string(for: n) ?? ""
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .ordinal
+    return formatter.string(for: n) ?? ""
 }
 
 
 class AppState: ObservableObject {
     @Published var count = 0
+    @Published var favoritePrimes: [Int] = []
 }
 
 struct CounterView: View {
     @ObservedObject var state: AppState
+    @State var isPrimeModalShown: Bool = false
     
-     var body: some View {
-         
+    var body: some View {
+        
         VStack {
             HStack {
                 Button {
@@ -50,16 +52,56 @@ struct CounterView: View {
                 }
             }
             
-            Button(action: {}) {
+            Button(action: { self.isPrimeModalShown = true }) {
                 Text("is this prime?")
             }
             Button(action: {}) {
                 Text("What is the \(ordinal(self.state.count)) prime?")
             }
-            
         }
         .font(.title)
         .navigationTitle("Counter Demo")
+        .sheet(isPresented: self.$isPrimeModalShown) {
+            IsPrimeModalView(state: self.state)
+        }
+    }
+}
+
+private func isPrime (_ p: Int) -> Bool {
+  if p <= 1 { return false }
+  if p <= 3 { return true }
+  for i in 2...Int(sqrtf(Float(p))) {
+    if p % i == 0 { return false }
+  }
+  return true
+}
+
+struct IsPrimeModalView: View {
+    @ObservedObject var state: AppState
+    
+    var body: some View {
+        VStack {
+            if isPrime(state.count) {
+                Text("\(state.count) is Prime!")
+                if state.favoritePrimes.contains(self.state.count) {
+                    Button {
+                        state.favoritePrimes
+                        .removeAll(where: { $0 == self.state.count })
+                    } label: {
+                        Text("Remove favorite primes")
+                    }
+                } else {
+                    Button {
+                        state.favoritePrimes
+                        .append(self.state.count)
+                    } label: {
+                        Text("Save favorite primes")
+                    }
+                }
+            } else {
+                Text("\(state.count) is Not Prime....")
+            }
+        }
     }
 }
 
