ad683533c3c38f799117d58046fad826b181c8bf
Jinsujin <sylpid003@gmail.com>
2023-04-03 22:47:09 +0900

    refac: pullback (#10)

--- ComposableArchitecture/ComposableArchitecture/ContentView.swift
+++ ComposableArchitecture/ComposableArchitecture/ContentView.swift
@@ -107,19 +107,15 @@ func combine<Value, Action>(
 // LocalValue -> GlobalValue
 func pullback<LocalValue, GlobalValue, Action>(
   _ reducer: @escaping (inout LocalValue, Action) -> Void,
-  get: @escaping (GlobalValue) -> LocalValue,
-  set: @escaping (inout GlobalValue, LocalValue) -> Void
+  value: WritableKeyPath<GlobalValue, LocalValue>
 ) -> (inout GlobalValue, Action) -> Void {
-
-  return  { globalValue, action in
-    var localValue = get(globalValue)
-    reducer(&localValue, action)
-    set(&globalValue, localValue)
+  return { globalValue, action in
+    reducer(&globalValue[keyPath: value], action)
   }
 }
 
 let appReducer = combine(
-    pullback(counterReducer, get: { $0.count }, set: { $0.count = $1 }),
+    pullback(counterReducer, value: \.count),
     primeModalReducer,
     favoritePrimesReducer
 )
